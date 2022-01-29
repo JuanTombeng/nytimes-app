85038358ac65b132fdfc59044b12a8cd4e53282a
Juan Tombeng <juantombeng03@gmail.com>
2022-01-30 02:03:49 +0700

    app.js config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import style from "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './Pages/Home/Home'
+import ArticleList from "./Pages/ArticleList/ArticleList";
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/articles' element={<ArticleList />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
